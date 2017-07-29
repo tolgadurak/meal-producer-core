ba2ed24f40505cd8535b38d7211e81dc000cf65b
Tolga Durak <dev.tolga@gmail.com>
2017-07-30 00:37:15 +0300

    Init project

---
+++ src/main/java/io/github/tolgadurak/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.github.tolgadurak;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MealProducerCoreApplication.class);
+	}
+
+}
